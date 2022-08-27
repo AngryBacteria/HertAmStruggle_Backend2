892bc804186db1f9f80a947ad073422b509df6e5
Nicolas Gujer <nicolas.gujer@students.bfh.ch>
2022-08-27 09:06:24 +0200

    Initial commit

---
+++ src/main/java/com/example/hertamstruggle_backend2/HertAmStruggleBackend2Application.java
@@ -0,0 +1,13 @@
+package com.example.hertamstruggle_backend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HertAmStruggleBackend2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HertAmStruggleBackend2Application.class, args);
+    }
+
+}
